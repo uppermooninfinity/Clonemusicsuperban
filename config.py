ce1ca7dcd1c7d9c7ffb174f0a0fd0c2a27d1e175
ARJUN <pachangedinanath@gmail.com>
2026-02-03 23:40:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "26950458"))
+API_HASH = getenv("API_HASH", "d818b8d530e4a9b209509815ab1b9c7c")
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN","")
@@ -25,11 +25,11 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 100000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", )
-GBAN_LOG_CHAT = int(getenv("GBAN_LOG_CHAT",  ))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002869205475" )
+GBAN_LOG_CHAT = int(getenv("GBAN_LOG_CHAT", "-1002869205475" ))
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", ""))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+OWNER_ID = int(getenv("OWNER_ID", "7487670897"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7487670897").split()))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
