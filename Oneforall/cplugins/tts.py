f5afe064da9eeed868000486d81e9cfa82651ca1
ARJUN <pachangedinanath@gmail.com>
2026-02-06 19:36:54 +0530

    Clone text to speech by ˹ ɪɴꜰɪɴɪᴛʏ ✘ ɴᴇᴛᴡᴏʀᴋ˼ 🎧

---
+++ Oneforall/cplugins/tts.py
@@ -0,0 +1,41 @@
+import io
+
+from gtts import gTTS
+from pyrogram import filters
+
+from Oneforall import app
+
+
+@app.on_message(filters.command("tts"))
+async def text_to_speech(client, message):
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "😴Please provide some text to convert to speech.🗣️"
+        )
+
+    text = message.text.split(None, 1)[1]
+    tts = gTTS(text, lang="hi")
+    audio_data = io.BytesIO()
+    tts.write_to_fp(audio_data)
+    audio_data.seek(0)
+
+    audio_file = io.BytesIO(audio_data.read())
+    audio_file.name = "audio.mp3"
+    await message.reply_audio(audio_file)
+
+
+__HELP__ = """<blockquote expandable>
+**✦🗣️ ᴛᴇxᴛ ᴛᴏ sᴘᴇᴇᴄʜ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅ**
+
+🌐ᴜsᴇ ᴛʜᴇ `/tts` ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ᴄᴏɴᴠᴇʀᴛ ᴛᴇxᴛ ɪɴᴛᴏ sᴘᴇᴇᴄʜ.
+
+- 🔍`/tts <ᴛᴇxᴛ>`: ᴄᴏɴᴠᴇʀᴛs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴛᴏ sᴘᴇᴇᴄʜ ɪɴ ʜɪɴᴅɪ.
+
+**🔗ᴇxᴀᴍᴘʟᴇ:**
+- 🔸`/tts Namaste Duniya`
+
+**🩷ɴᴏᴛᴇ:**
+♻️ᴍᴀᴋᴇ sᴜʀᴇ ᴛᴏ ᴘʀᴏᴠɪᴅᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴀғᴛᴇʀ ᴛʜᴇ `/tts` ᴄᴏᴍᴍᴀɴᴅ.
+</blockquote expandable>"""
+
+__MODULE__ = "Tᴛs"
