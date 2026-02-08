85c303abb1943634d78940f60896d162e3d92a26
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-09 02:13:50 +0530

    Update start.py

--- Oneforall/utils/inline/start.py
+++ Oneforall/utils/inline/start.py
@@ -32,7 +32,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-                InlineKeyboardButton(text=_["C_B_1"], callback_data="help_callback chelp"),
-       ],
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
+        ],
     ]
     return buttons
