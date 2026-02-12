f5c6d79075cee24babed69c3fe7678c309934872
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-12 23:37:02 +0530

    Update logger.py

--- Oneforall/utils/logger.py
+++ Oneforall/utils/logger.py
@@ -7,19 +7,20 @@ from Oneforall.utils.database import is_on_off
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
-        logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+        logger_text = f"""<blockquote><i>
+<b><u>{app.mention} ηєᴡ ꜱᴛʀєᴧϻ ʀєQᴜєꜱᴛ ɪᴅєηᴛɪꜰɪєᴅ 🚀🎶</u></b>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+<b>ɢʀσᴜᴘ ᴜηɪQᴜє ɪᴅ 🆔✨ :</b> <code>{message.chat.id}</code>
+<b>ɢʀσᴜᴘ ηᴧϻє 👥✨ :</b> {message.chat.title}
+<b>ɢʀσᴜᴘ ᴜꜱєʀηᴧϻє 🌐✨:</b> @{message.chat.username}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>ᴜꜱєʀ ɪᴅ 🆔✨ :</b> <code>{message.from_user.id}</code>
+<b>ηᴧϻє 🌿✨ :</b> {message.from_user.mention}
+<b>ᴅєᴛєᴄᴛєᴅ ᴜꜱєʀηᴧϻє 🛰️✨ :</b> @{message.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+<b>ꜱєᴧʀᴄʜ 🔎✨:</b> {message.text.split(None, 1)[1]}
+<b>ꜱᴛʀєᴧϻ ʙʏᴘᴧꜱꜱ ᴛʜʀσᴜɢʜ ⚡🎶:</b> {streamtype}</i></blockquote>"""
+    
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
@@ -31,3 +32,34 @@ async def play_logs(message, streamtype):
             except:
                 pass
         return
+
+async def clone_bot_logs(client, message, bot_mention, clone_logger_id, streamtype):
+
+    if not clone_logger_id:
+        print("[ERROR] clone_logger_id is missing!")
+        return
+
+    logger_text = f"""<blockquote><i>
+<b><u>{bot_mention} ηєᴡ ꜱᴛʀєᴧϻ ʀєQᴜєꜱᴛ ɪᴅєηᴛɪꜰɪєᴅ 🚀🎶</u></b>
+
+<b>ɢʀσᴜᴘ ᴜηɪQᴜє ɪᴅ 🆔✨ :</b> <code>{message.chat.id}</code>
+<b>ɢʀσᴜᴘ ηᴧϻє 👥✨ :</b> {message.chat.title}
+<b>ɢʀσᴜᴘ ᴜꜱєʀηᴧϻє 🌐✨:</b> @{message.chat.username}
+
+<b>ᴜꜱєʀ ɪᴅ 🆔✨ :</b> <code>{message.from_user.id}</code>
+<b>ηᴧϻє 🌿✨ :</b> {message.from_user.mention}
+<b>ᴅєᴛєᴄᴛєᴅ ᴜꜱєʀηᴧϻє 🛰️✨ :</b> @{message.from_user.username}
+
+<b>ꜱєᴧʀᴄʜ 🔎✨:</b> {message.text.split(None, 1)[1]}
+<b>ꜱᴛʀєᴧϻ ʙʏᴘᴧꜱꜱ ᴛʜʀσᴜɢʜ ⚡🎶:</b> {streamtype}</i></blockquote>"""
+    
+    if message.chat.id != clone_logger_id:
+        try:
+            await client.send_message(
+                chat_id=int(clone_logger_id),
+                text=logger_text,
+                parse_mode=ParseMode.HTML,
+                disable_web_page_preview=True,
+            )
+        except Exception as e:
+            print(f"[ERROR] Clone Bot Log Failed ({bot_mention}): {e}")
